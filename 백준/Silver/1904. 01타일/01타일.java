60bf474b7cf30603b7b5f0b806dcbf19282f6040
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-03-01 15:21:41 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 18064 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+    public void run() {
+        FastReader fr = new FastReader();
+                int n = Integer.parseInt(fr.next());
+        this.n = n;
+        System.out.println(this.solve(n));
+    }
+    public int n;
+    public int[] dp = new int[1000001];
+
+    /**
+     * 문제 해결 메서드
+     * 
+     * @param 문제에 맞는 파라미터들
+     * @return 문제에 맞는 반환 타입
+     */
+    public int solve(int n) {
+        // TODO: 여기에 알고리즘 구현
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+        return dp[n];
+    }
+
+    static class FastReader {
+        BufferedReader br; StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public String next() { while (st == null || !st.hasMoreElements()) { try { st = new StringTokenizer(br.readLine()); } catch (IOException e) { } } return st.nextToken(); }
+        public int nextInt() { return Integer.parseInt(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+        public boolean hasMoreTokens() { while (st == null || !st.hasMoreElements()) { String line = null; try { line = br.readLine(); } catch (IOException e) { } if (line == null) return false; st = new StringTokenizer(line); } return true; }
+    }
+}
