28134ecea7022d71dec0abb87d5b893e0cdf38ad
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-03-22 15:01:06 +0900

    [Silver IV] Title: 수들의 합 2, Time: 140 ms, Memory: 15192 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+    public void run() {
+        FastReader fr = new FastReader();
+                int n = Integer.parseInt(fr.next());
+        int m = Integer.parseInt(fr.next());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(fr.next());
+        System.out.println(this.solve(n, m, arr));
+    }
+    /**
+     * 문제 해결 메서드
+     * 
+     * @param 문제에 맞는 파라미터들
+     * @return 문제에 맞는 반환 타입
+     */
+    public int solve(int n, int m, int[] arr) {
+        int start = 0;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            while (sum > m) {
+                sum -= arr[start];
+                start++;
+            }
+
+            if (sum == m) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static class FastReader {
+        BufferedReader br; StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public String next() { while (st == null || !st.hasMoreElements()) { try { st = new StringTokenizer(br.readLine()); } catch (IOException e) { } } return st.nextToken(); }
+        public int nextInt() { return Integer.parseInt(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+        public boolean hasMoreTokens() { while (st == null || !st.hasMoreElements()) { String line = null; try { line = br.readLine(); } catch (IOException e) { } if (line == null) return false; st = new StringTokenizer(line); } return true; }
+    }
+}
