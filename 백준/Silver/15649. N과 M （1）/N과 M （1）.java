31d29307cbdd8b07037e67a31dc06a9872e2c31e
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-03-24 12:42:29 +0900

    [Silver III] Title: N과 M (1), Time: 1628 ms, Memory: 51364 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        FastReader fr = new FastReader();
+        int n = Integer.parseInt(fr.next());
+        int m = Integer.parseInt(fr.next());
+        this.solve(n, m);
+    }
+
+    boolean[] visited;
+    int[] res;
+
+    int n;
+    int m;
+
+    /**
+     * 문제 해결 메서드
+     * 
+     * @param 문제에 맞는 파라미터들
+     * @return 문제에 맞는 반환 타입
+     */
+    public int solve(int n, int m) {
+        this.n = n;
+        this.m = m;
+        this.res = new int[m];
+        this.visited = new boolean[n + 1];
+
+        dfs(0);
+
+        return 0;
+    }
+
+    public void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i])
+                continue;
+
+            res[depth] = i + 1;
+            visited[i] = true;
+            dfs(depth + 1);
+            visited[i] = false;
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public boolean hasMoreTokens() {
+            while (st == null || !st.hasMoreElements()) {
+                String line = null;
+                try {
+                    line = br.readLine();
+                } catch (IOException e) {
+                }
+                if (line == null)
+                    return false;
+                st = new StringTokenizer(line);
+            }
+            return true;
+        }
+    }
+}
