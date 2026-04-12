a616a443fc84aa70918bd7ab0254b0c235db504b
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-04-12 16:12:49 +0900

    [Silver III] Title: N과 M (3), Time: 384 ms, Memory: 67984 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+    public void run() {
+        FastReader fr = new FastReader();
+                int n = Integer.parseInt(fr.next());
+        int m = Integer.parseInt(fr.next());
+        this.solve(n, m);
+    }
+    int n;
+    int m;
+    int[] arr;
+    StringBuilder sb = new StringBuilder();
+
+    public void solve(int n, int m) {
+        this.n = n;
+        this.m = m;
+        this.arr = new int[m];
+
+        dfs(0);
+        
+        System.out.print(sb.toString());
+    }
+
+    public void dfs(int index) {
+        if (index == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[index] = i;
+            dfs(index + 1);
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br; StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public String next() { while (st == null || !st.hasMoreElements()) { try { st = new StringTokenizer(br.readLine()); } catch (IOException e) { } } return st.nextToken(); }
+        public int nextInt() { return Integer.parseInt(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+        public boolean hasMoreTokens() { while (st == null || !st.hasMoreElements()) { String line = null; try { line = br.readLine(); } catch (IOException e) { } if (line == null) return false; st = new StringTokenizer(line); } return true; }
+    }
+}
