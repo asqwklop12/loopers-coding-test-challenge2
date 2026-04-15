999888e2f03fd34d9b1c158eba22141857728401
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-04-15 10:41:39 +0900

    [Silver V] Title: 배열 합치기, Time: 1332 ms, Memory: 312604 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        FastReader fr = new FastReader();
+        int n = Integer.parseInt(fr.next());
+        int m = Integer.parseInt(fr.next());
+        int[] a = new int[n];
+        for (int j = 0; j < n; j++)
+            a[j] = Integer.parseInt(fr.next());
+        int[] b = new int[m];
+        for (int j = 0; j < m; j++)
+            b[j] = Integer.parseInt(fr.next());
+        this.solve(n, m, a, b);
+    }
+
+    /**
+     * 문제 해결 메서드
+     * 
+     * @param 문제에 맞는 파라미터들
+     * @return 문제에 맞는 반환 타입
+     */
+
+    public void solve(int n, int m, int[] a, int[] b) {
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+
+        while (i < n && j < m) {
+            if (a[i] <= b[j]) {
+                sb.append(a[i++]).append(" ");
+            } else {
+                sb.append(b[j++]).append(" ");
+            }
+        }
+
+        while (i < n) {
+            sb.append(a[i++]).append(" ");
+        }
+
+        while (j < m) {
+            sb.append(b[j++]).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public boolean hasMoreTokens() {
+            while (st == null || !st.hasMoreElements()) {
+                String line = null;
+                try {
+                    line = br.readLine();
+                } catch (IOException e) {
+                }
+                if (line == null)
+                    return false;
+                st = new StringTokenizer(line);
+            }
+            return true;
+        }
+    }
+}
